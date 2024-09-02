e4ac9a81c7f1896c1c2032ecb64853e6dfea26cc
Vasteron-git <Mp.selvester@gmail.com>
2024-09-02 19:15:25 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,11 @@
+from typing import List, Any
+
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+len_my_list = len(my_list) - 1
+i = 0
+while i <= len_my_list:
+    if my_list[i] < 0:
+        break
+    else:
+        print(my_list[i])
+    i = i + 1
