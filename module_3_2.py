5b5697bbb456000cb797fb43644983ee8399f9d4
Vasteron-git <Mp.selvester@gmail.com>
2024-09-09 19:33:19 +0300

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -1,5 +1,5 @@
 
-def send_email(message, recipient, sender = "university.help@gmail.com"):
+def send_email(message, recipient, *, sender = "university.help@gmail.com"):
     if recipient == sender:
         print("Нельзя отправить письмо самому себе!")
         return
