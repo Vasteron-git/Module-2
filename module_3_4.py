205fa757d6502d1e2a7ecb9e19b0675391281551
Vasteron-git <Mp.selvester@gmail.com>
2024-09-11 19:47:47 +0300

    Add files via upload

---
+++ module_3_4.py
@@ -0,0 +1,12 @@
+
+def single_root_words(root_word, *other_words):
+    same_words = []
+    # if any(string.lower() == item.lower() for item in list_to_search):
+    for i in range(0, len(other_words)):
+        if root_word.lower() in other_words[i].lower() or other_words[i].lower() in root_word.lower():
+            same_words.append(other_words[i])
+    return same_words
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result1)
+print(result2)
